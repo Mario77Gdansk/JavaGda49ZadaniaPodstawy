082f504ed5f66ee47951e0cf8fdc208f8201c25c
Mariusz <mariusz@kursGIT.pl>
2021-09-11 14:27:27 +0200

    Task 7

---
+++ src/tasksDayNo01/Task7.java
@@ -0,0 +1,65 @@
+package tasksDayNo01;
+
+import java.util.Scanner;
+
+public class Task7 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String decision;
+//        float a = scanner.nextFloat();
+//        char mathOperator = getTheSign();
+//        float b = scanner.nextFloat();
+
+        do {
+            System.out.println("Please insert first digit: ");
+            float firstDigit = scanner.nextFloat();
+            System.out.println("Please insert second digit: ");
+            float secondDigit = scanner.nextFloat();
+            System.out.println("Please insert operator: ");
+            scanner.nextLine();
+            String operator = scanner.nextLine();
+
+            float result = calculate(firstDigit, secondDigit, operator);
+            System.out.println("Result: \n" + result);
+
+
+            System.out.println("New calculation or STOP");
+            decision = scanner.nextLine();
+            
+            } while ("STOP".equalsIgnoreCase(decision));
+
+//        swich(mathOperator = '+');2345
+//        {
+//            System.out.println(a&" + "b&" = "a+b);
+//        }
+//
+//
+//        System.out.println();
+    }
+
+    private static float calculate(float firstDigit, float secondDigit, String operator) {
+        float result = 0;
+        switch (operator) {
+            case "+":
+                return firstDigit + secondDigit;
+            case "-":
+                return firstDigit - secondDigit;
+            case "*":
+                return firstDigit * secondDigit;
+            case "/":
+                if (secondDigit==0){
+                    System.out.println("ERROR");
+                    System.exit(1);
+                }
+                return firstDigit / secondDigit;
+            case "^":
+                return (float)Math.pow(firstDigit,secondDigit);
+            case "sqrt":
+                return (float)Math.pow(firstDigit,(1/secondDigit));
+            default:
+                System.out.println("operator not exist");
+                System.exit(1);
+        }
+        return result;
+    }
+}
